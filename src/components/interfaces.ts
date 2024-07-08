386327c25294a380bca88a6cae1db3c8a118b19c
Lorenzo <hello@lorenzo0111.me>
2024-07-08 17:51:12 +0200

    Rewrote audio system

--- src/components/interfaces.ts
+++ src/components/interfaces.ts
@@ -1,7 +1,4 @@
-import type {
-  MediaAudioTrackConstraints,
-  recorderControls,
-} from "../hooks/useAudioRecorder";
+import type { recorderControls } from "../hooks/useAudioRecorder";
 
 interface StyleProps {
   /**
@@ -40,27 +37,13 @@ export interface Props {
    * This gets called when the getUserMedia Promise is rejected.
    * It takes the resultant DOMException as its parameter.
    **/
-  onNotAllowedOrFound?: (exception: DOMException) => void;
+  onNotAllowedOrFound?: (exception: CaptureError) => void;
   /**
    * Allows calling of hook outside this component. The controls returned by the hook can then be passed to the component using this prop.
    * This allows for use of hook methods and state outside this component
    * @sample_usage https://github.com/samhirtarif/react-audio-recorder#combine-the-useaudiorecorder-hook-and-the-audiorecorder-component
    **/
   recorderControls?: recorderControls;
-  /**
-   * Takes a {@link https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSettings#instance_properties_of_audio_tracks subset} of
-   * `MediaTrackConstraints` that apply to the audio track
-   *
-   * @Property `deviceId`
-   * @Property `groupId`
-   * @Property `autoGainControl`
-   * @Property `channelCount`
-   * @Property `echoCancellation`
-   * @Property `noiseSuppression`
-   * @Property `sampleRate`
-   * @Property `sampleSize`
-   */
-  audioTrackConstraints?: MediaAudioTrackConstraints;
   /**
    * If set to `true` the file gets downloaded when save recording is pressed
    **/
@@ -69,14 +52,6 @@ export interface Props {
    * File extension for the audio filed that gets downloaded
    **/
   downloadFileExtension?: "mp3" | "wav" | "webm";
-  /**
-   * Displays a waveform visualization for the audio when set to `true`. Defaults to `false`
-   **/
-  showVisualizer?: boolean;
-  /**
-   * The options passed to the HTML MediaRecorder API.
-   **/
-  mediaRecorderOptions?: MediaRecorderOptions;
   /**
    * Custom classes to changes styles.
    **/
--- src/hooks/useAudioRecorder.ts
+++ src/hooks/useAudioRecorder.ts
@@ -3,54 +3,27 @@ import { useState, useCallback } from "react";
 export interface recorderControls {
   startRecording: () => void;
   stopRecording: () => void;
-  togglePauseResume: () => void;
   recordingBlob?: Blob;
   isRecording: boolean;
-  isPaused: boolean;
   recordingTime: number;
-  mediaRecorder?: MediaRecorder;
 }
-
-export type MediaAudioTrackConstraints = Pick<
-  MediaTrackConstraints,
-  | "deviceId"
-  | "groupId"
-  | "autoGainControl"
-  | "channelCount"
-  | "echoCancellation"
-  | "noiseSuppression"
-  | "sampleRate"
-  | "sampleSize"
->;
 
 /**
  * @returns Controls for the recording. Details of returned controls are given below
  *
- * @param `audioTrackConstraints`: Takes a {@link https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackSettings#instance_properties_of_audio_tracks subset} of `MediaTrackConstraints` that apply to the audio track
  * @param `onNotAllowedOrFound`: A method that gets called when the getUserMedia promise is rejected. It receives the DOMException as its input.
  *
  * @details `startRecording`: Calling this method would result in the recording to start. Sets `isRecording` to true
  * @details `stopRecording`: This results in a recording in progress being stopped and the resulting audio being present in `recordingBlob`. Sets `isRecording` to false
- * @details `togglePauseResume`: Calling this method would pause the recording if it is currently running or resume if it is paused. Toggles the value `isPaused`
  * @details `recordingBlob`: This is the recording blob that is created after `stopRecording` has been called
  * @details `isRecording`: A boolean value that represents whether a recording is currently in progress
- * @details `isPaused`: A boolean value that represents whether a recording in progress is paused
  * @details `recordingTime`: Number of seconds that the recording has gone on. This is updated every second
- * @details `mediaRecorder`: The current mediaRecorder in use
  */
 const useAudioRecorder: (
-  audioTrackConstraints?: MediaAudioTrackConstraints,
-  onNotAllowedOrFound?: (exception: DOMException) => void,
-  mediaRecorderOptions?: MediaRecorderOptions
-) => recorderControls = (
-  audioTrackConstraints,
-  onNotAllowedOrFound,
-  mediaRecorderOptions
-) => {
+  onNotAllowedOrFound?: (exception: CaptureError) => void
+) => recorderControls = (onNotAllowedOrFound) => {
   const [isRecording, setIsRecording] = useState(false);
-  const [isPaused, setIsPaused] = useState(false);
   const [recordingTime, setRecordingTime] = useState(0);
-  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder>();
   const [timerInterval, setTimerInterval] = useState<NodeJS.Timeout>();
   const [recordingBlob, setRecordingBlob] = useState<Blob>();
 
@@ -72,81 +45,66 @@ const useAudioRecorder: (
   const startRecording: () => void = useCallback(() => {
     if (timerInterval != null) return;
 
-    navigator.mediaDevices
-      .getUserMedia({ audio: audioTrackConstraints ?? true })
-      .then((stream) => {
-        setIsRecording(true);
-        const recorder: MediaRecorder = new MediaRecorder(
-          stream,
-          mediaRecorderOptions
-        );
-        setMediaRecorder(recorder);
-        recorder.start();
-        _startTimer();
+    navigator.device.capture.captureAudio(
+      (files) => {
+        const file = files[0];
+        const path = file.fullPath;
 
-        recorder.addEventListener("dataavailable", (event) => {
-          setRecordingBlob(event.data);
-          recorder.stream.getTracks().forEach((t) => t.stop());
-          setMediaRecorder(undefined);
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const blob = new Blob(
+            [new Uint8Array(reader.result as ArrayBuffer)],
+            {
+              type: "audio/wav",
+            }
+          );
+          setRecordingBlob(blob);
+        };
+
+        fetch(path).then((response) => {
+          response.blob().then((blob) => {
+            reader.readAsArrayBuffer(blob);
+          });
         });
-      })
-      .catch((err: DOMException) => {
-        console.log(err.name, err.message, err.cause);
+
+        setIsRecording(false);
+        _stopTimer();
+        setRecordingTime(0);
+        return;
+      },
+      (err) => {
+        console.log(err.message);
         onNotAllowedOrFound?.(err);
-      });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+      }
+    );
+
+    setIsRecording(true);
+    _startTimer();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [
     timerInterval,
     setIsRecording,
-    setMediaRecorder,
     _startTimer,
     setRecordingBlob,
     onNotAllowedOrFound,
-    mediaRecorderOptions,
   ]);
 
   /**
    * Calling this method results in a recording in progress being stopped and the resulting audio being present in `recordingBlob`. Sets `isRecording` to false
    */
   const stopRecording: () => void = useCallback(() => {
-    mediaRecorder?.stop();
     _stopTimer();
     setRecordingTime(0);
     setIsRecording(false);
-    setIsPaused(false);
-  }, [
-    mediaRecorder,
-    setRecordingTime,
-    setIsRecording,
-    setIsPaused,
-    _stopTimer,
-  ]);
-
-  /**
-   * Calling this method would pause the recording if it is currently running or resume if it is paused. Toggles the value `isPaused`
-   */
-  const togglePauseResume: () => void = useCallback(() => {
-    if (isPaused) {
-      setIsPaused(false);
-      mediaRecorder?.resume();
-      _startTimer();
-    } else {
-      setIsPaused(true);
-      _stopTimer();
-      mediaRecorder?.pause();
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [mediaRecorder, setIsPaused, _startTimer, _stopTimer]);
+  }, [setRecordingTime, setIsRecording, _stopTimer]);
 
   return {
     startRecording,
     stopRecording,
-    togglePauseResume,
     recordingBlob,
     isRecording,
-    isPaused,
     recordingTime,
-    mediaRecorder,
   };
 };
 
